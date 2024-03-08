c7cabaabdfdaa43f539b293e5d7ff2c833159c3e
liwjln <phamtrantuantu17@gmail.com>
2024-03-08 11:04:02 +0700

    Add underline

--- app/src/main/java/com/deadlineshooters/studentmanagementapp/EditStudentActivity.kt
+++ app/src/main/java/com/deadlineshooters/studentmanagementapp/EditStudentActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -11,6 +13,7 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.text.HtmlCompat
 import com.deadlineshooters.studentmanagementapp.databinding.ActivityEditStudentBinding
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -159,6 +162,8 @@ class EditStudentActivity : AppCompatActivity() {
             intent.putExtra("selectedClass", binding.classButton.text.toString())
             resultLauncher.launch(intent)
         }*/
+        val html = "(<u>html link</u>)"
+        binding.gotoWebview.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_LEGACY)
 
         binding.gotoWebview.setOnClickListener {
             val intent = Intent(this, WebViewActivity::class.java).apply {
